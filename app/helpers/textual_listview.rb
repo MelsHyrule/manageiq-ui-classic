d4b970003eb52f8f8f98e24d379f9aab1a3eecdc
Melody-Ann-Seda-Marotte <Melody.Ann.Seda.Marotte@ibm.com>
2021-07-15 13:27:54 -0400

    title default "Variables"

--- app/helpers/textual_listview.rb
+++ app/helpers/textual_listview.rb
@@ -1,7 +1,7 @@
 TextualListview = Struct.new(:title, :headers, :col_order, :value) do
   def locals
     {
-      :title     => title.nil? ? "" : title,
+      :title     => title.nil? ? "Variables" : title,
       :headers   => headers.nil? ? ["Name", "Value"] : headers,
       :colOrder  => col_order.nil? ? ["name", "value"] : col_order,
       :values    => value.nil? ? [] : value,
