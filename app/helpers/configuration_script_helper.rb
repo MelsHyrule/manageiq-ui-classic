a6ce6cef61db4706b81f4a786a4251fcb09c603b
Melody-Ann-Seda-Marotte <Melody.Ann.Seda.Marotte@ibm.com>
2021-07-15 12:57:21 -0400

    nil case works now

--- app/helpers/configuration_script_helper.rb
+++ app/helpers/configuration_script_helper.rb
@@ -79,11 +79,36 @@ module ConfigurationScriptHelper
     {:label => _("Region"), :value => @record.region_description}
   end
 
+  def mels
+    p "printing textual_configuration_script_variables #{textual_configuration_script_variables()}"
+    textual_configuration_script_variables()
+    ## show.html.haml is not built to receive a []
+
+    ##"printing textual_configuration_script_variables {:title=>\"Variables\", :headers=>[\"Name\", \"Value\"], 
+    ## :col_order=>[\"name\", \"value\"], :value=>[{:name=>\"win_update_cat_names\", 
+    ## :value=>\"[\\\"CriticalUpdates\\\", \\\"SecurityUpdates\\\"]\"}, {:name=>\"ansible_user\", :value=>\"cloud-user\"}, 
+    ## {:name=>\"ansible_password\", :value=>\"Passw0rd\"}, {:name=>\"ansible_port\", :value=>\"5985\"}, {:name=>\"ansible_connection\", :value=>\"winrm\"}]}"
+  end
+
   def textual_configuration_script_variables
-    textual_variables(@record.variables) unless @record.variables.nil?
+    p "memememememee"
+    #<ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationWorkflow id: 466, manager_id: 19, manager_ref: nil, name: nil, description: nil, variables: nil, created_at: "2021-07-09 17:36:25", updated_at: "2021-07-09 17:36:25", survey_spec: nil, inventory_root_group_id: nil, type: "ManageIQ::Providers::AnsibleTower::AutomationManag...", parent_id: nil, configuration_script_source_id: nil>
+
+    p "(@record) #{@record}" ## #<ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationScript:0x00007fe1f3b3cb08>
+    p "textual_variables(@record.variables) #{textual_variables(@record.variables)}"
+    textual_variables(@record.variables)
+
+    ## working case
+    # [] # if this function returns [] if throws exception
+    #fails with --> no implicit conversion of Symbol into Integer [configuration_script/show]
+
+    ## "textual_variables(@record.variables) {:title=>\"Variables\", :headers=>[\"Name\", \"Value\"], :col_order=>[\"name\", \"value\"], :value=>[]}"
+
   end
 
   def textual_configuration_script_survey
+    p "but does it make it inside?" 
+    ## we dont even make it here on a working case
     textual_survey_group(@record.survey_spec['spec'])
   end
 
@@ -116,21 +141,48 @@ module ConfigurationScriptHelper
   end
 
   def textual_variables(vars)
-    return unless vars
+    m = {:title=> _("Variables"), :headers=>[_('Name'), _('Value')], :col_order=>%w[name value], :value=>[{:name=>_("Name"), :value=>_("Name")}]}
+    ## undefined method `values_at' for #<String:0x00007f9616ad63b8> [configuration_script/show]
+    p "vars mels #{vars}" ## empty
+    
+    # return unless vars ## gives: undefined method `values_at' for nil:NilClass [configuration_script/show]
+    
+    # return [] if vars.nil? ## gives: no implicit conversion of Symbol into Integer [configuration_script/show]
+    
+    return {} if vars.nil?
+
+    # return m if vars.nil?
+
+    ## none of the below runs if vars is nil 
+
+    ## bye bug vars holds vars
+# {"template_file_proxy"=>"haproxy.cfg.j2", "proxy_config_file_path"=>"/etc/haproxy/haproxy.cfg"}
     h = {:title     => _("Variables"),
          :headers   => [_('Name'), _('Value')],
          :col_order => %w[name value]}
-    h[:value] = vars.collect do |item|
+
+         ##"textual_variables mels mels passed return"
+
+         ##undefined method `values_at' for "textual_variables(@record.variables) ":String [configuration_script/show]
+    p "TESTING MY CODE"
+    p "Array(vars) #{Array(vars)}"
+    h[:value] = vars.collect do |item| ## in a nil case, item will be nothing, there will be no item[0] to apply to_s to 
+    # h[:value] = Array(vars).collect do |item| ## here it breaks still 
       {
         :name  => item[0].to_s,
         :value => item[1].to_s
       }
     end
+    p "h is mels #{h}"
+    ## working case "h is mels {:title=>\"Variables\", :headers=>[\"Name\", \"Value\"], :col_order=>[\"name\", \"value\"], :value=>[]}"
+    ##              "h is mels {:title=>\"Variables\", :headers=>[\"Name\", \"Value\"], :col_order=>[\"name\", \"value\"], :value=>[{:name=>\"packages\", :value=>\"httpd\"}]}"
     h
   end
+  #"h is mels {:title=>\"Variables\", :headers=>[\"Name\", \"Value\"], :col_order=>[\"name\", \"value\"], :value=>[]}"
+  #undefined method `values_at' for #<String:0x00007f961abc6d00> [configuration_script/show]
 
   def textual_group_smart_management
-    TextualTags.new(_("Smart Management"), %i[tags])
+    TextualTags.new(_("Smart Mels Management"), %i[tags])  ##Smart Management this is what is loaded 
   end
 end
 #
