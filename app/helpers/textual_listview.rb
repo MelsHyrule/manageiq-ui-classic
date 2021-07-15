3600c69cdc5e348a8fb932901d2312421dd670fa
Melody-Ann-Seda-Marotte <Melody.Ann.Seda.Marotte@ibm.com>
2021-07-15 13:23:04 -0400

    cleaned out code and still works

--- app/helpers/textual_listview.rb
+++ app/helpers/textual_listview.rb
@@ -1,18 +1,6 @@
 TextualListview = Struct.new(:title, :headers, :col_order, :value) do
   def locals
-    #here is the incoming data being null
-    p "mels TextualListview title #{title}"
-    p "mels TextualListview headers #{headers}"
-    p "mels TextualListview col_order #{col_order}"
-    p "mels TextualListview value #{value}"
-
-    ## all these values equaling nil breaks our party
-
-# "mels TextualListview title Variables"
-# "mels TextualListview headers [\"Name\", \"Value\"]"
-# "mels TextualListview col_order [\"name\", \"value\"]"
-# "mels TextualListview value []"
-    m = {
+    {
       :title     => title.nil? ? "" : title,
       :headers   => headers.nil? ? ["Name", "Value"] : headers,
       :colOrder  => col_order.nil? ? ["name", "value"] : col_order,
@@ -20,10 +8,6 @@ TextualListview = Struct.new(:title, :headers, :col_order, :value) do
       :rowLabel  => _('View the table'),
       :component => 'TableListView',
     }
-    p "mels m #{m}"
-    m
-
-    ##"mels m {:title=>\"Variables\", :headers=>[\"Name\", \"Value\"], :colOrder=>[\"name\", \"value\"], :values=>[], :rowLabel=>\"View the table\", :component=>\"TableListView\"}"
   end
 
   def self.new_from_hash(h)
